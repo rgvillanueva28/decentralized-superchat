e5a2b36d9ef4676897e7dacd961b3bfcbf318915
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2022-11-10 10:18:38 +0800

    Add tailwind Add flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
